2a0ee7ecc64ef27c426bbd8ac160d809d6431ab3
Olasunkanmi <abdulsalamquadri999@gmail.com>
2021-11-27 21:58:51 -0100

    Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+interface Note {
+    id: string,
+    title: string,
+    text: string,
+    color: string,
+    date: string
+}
 
 function App() {
+  const [notes, setNotes] = useState<Note[]>([{
+    id: (new Date).toString(),
+    title: "Meetings",
+    text: "Schedule meetings with designers",
+    color: "#ddd",
+    date: (new Date).toString()
+  }]);
+
   return (
     <div>
-      START!
+      APP 
     </div>
   );
 }
